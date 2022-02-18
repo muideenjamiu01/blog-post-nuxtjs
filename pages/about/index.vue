d7990446f27cefdf84755e5ae4101379257df6ac
yvMuhammed <jamiu@youverify.co>
2022-02-18 10:19:32 +0100

    crate a post page

---
+++ pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about-page">
+        <h1>this is my about page</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .about-page{
+        text-align: center;
+        padding: 30px ;
+    }
+</style>
